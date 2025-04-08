43619652469e4dd8125d8d9e244a987c8704b398
Edilson Schmitz Junior <116024288+Edilson-Schmitz-jr@users.noreply.github.com>
2025-04-08 09:40:19 -0300

    Primeiro Commit
    Versão inicial, Primeira e segunda páginas estão em beta, terceira está em Alfa. Restante em base primária de estruturação e sem elementos visuais sólidos.

---
+++ scripts.js
@@ -0,0 +1,38 @@
+const pages = document.querySelectorAll('.page');
+        const navButtons = document.getElementById('navButtons');
+        let currentPage = 0;
+
+        function showPage(index) {
+            pages.forEach((page, i) => {
+                page.classList.toggle('hidden', i !== index);
+            });
+            currentPage = index;
+            updateButtons();
+        }
+
+        function updateButtons() {
+            navButtons.innerHTML = '';
+            if (currentPage > 0) {
+                const prevButton = document.createElement('button');
+                prevButton.innerHTML = '<img class="imgNavButtons" src="Assets/img/SetaEsquerda.png" alt="Voltar" />';
+                prevButton.onclick = () => showPage(currentPage - 1);
+                navButtons.appendChild(prevButton);
+            }
+            if (currentPage < pages.length - 1) {
+                const nextButton = document.createElement('button');
+                const descubraButton = document.querySelector('#buttonDescubra');
+                nextButton.innerHTML = '<img class="imgNavButtons" src="Assets/img/SetaDireita.png" alt="Próxima" />';
+                nextButton.onclick = () => showPage(currentPage + 1);
+                navButtons.appendChild(nextButton);
+                descubraButton.onclick = () => showPage(currentPage + 1);
+            }
+            if (currentPage === pages.length - 1) {
+                const restartButton = document.createElement('button');
+                restartButton.innerHTML = '<img class="imgNavButtons" src="Assets/img/SetaReiniciar.png" alt="Reiniciar" />';
+                restartButton.onclick = () => showPage(0);
+                navButtons.appendChild(restartButton);
+            }
+        }
+        
+
+        showPage(0);
